4ad16835b5efa510696df170fc0b60c76fbb2497
John Demirci <ucjdemirci@gmail.com>
2024-08-26 09:50:19 -0700

    Initial Commit

---
+++ JDSimWatch/JDSimWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  JDSimWatchApp.swift
+//  JDSimWatch
+//
+//  Created by John Demirci on 8/26/24.
+//
+
+import SwiftUI
+
+@main
+struct JDSimWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
